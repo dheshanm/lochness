b6df0d9c07359ecf9df2fa06a4f37319f250fa07
kcho <kevincho@bwh.harvard.edu>
2025-07-23 22:47:11 -0400

    feat: test import env setup

--- test/lochness/tasks/test_push_data.py
+++ test/lochness/tasks/test_push_data.py
@@ -1,6 +1,25 @@
-from lochness.tasks import push_data
-from test_module import fake_data_fixture
+import sys
+from pathlib import Path
+
+for x in sys.path:
+    if 'lochness' in x:
+        sys.path.remove(x)
+
+file = Path(__file__).resolve()
+parent = file.parent
+root_dir = None
+for parent in file.parents:
+    if parent.name == "lochness_v2":
+        root_dir = parent
+
+print(root_dir)
+sys.path.append(str(root_dir))
+sys.path.append(str(root_dir / 'test'))
+
+from lochness.tasks.push_data import push_all_data
+from test_module import fake_data_fixture, config_file
 
 
 def test_example(fake_data_fixture):
     PROJECT_ID, PROJECT_NAME, SITE_ID, SITE_NAME, SUBJECT_ID = fake_data_fixture
+    push_all_data(config_file, PROJECT_ID, SITE_ID)
